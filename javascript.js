6abbb10334ed92108c4ea8e7e5b7e9b6dad2f851
Robi946 <179209963+Robi946@users.noreply.github.com>
2026-09-05 15:08:17 +0300

    Add logic for Human move choice

--- javascript.js
+++ javascript.js
@@ -4,3 +4,8 @@ function getComputerChoice() {
     else if (randNum >= 0.33 && randNum <= 0.66) return "paper";
     else if (randNum > 0.66) return "scissors";
 }
+
+function getHumanChoice() {
+    let input = prompt("Please enter the move you want to make: ", "rock");
+    return input;
+}

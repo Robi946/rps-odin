1743bdeb20673d87ce276c8c415d3b15585b83c8
Robi946 <179209963+Robi946@users.noreply.github.com>
2026-09-05 14:57:21 +0300

    Add computer shape choice decisionmaking

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    let randNum = Math.random();
+    if (randNum < 0.33) return "rock";
+    else if (randNum >= 0.33 && randNum <= 0.66) return "paper";
+    else if (randNum > 0.66) return "scissors";
+}

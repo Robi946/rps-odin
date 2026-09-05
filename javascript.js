fa9fb3e9e83f10ce263564badda58daf71ce0641
Robi946 <179209963+Robi946@users.noreply.github.com>
2026-09-05 16:03:06 +0300

    Add logic for a single round

--- javascript.js
+++ javascript.js
@@ -16,3 +16,45 @@ function getHumanChoice() {
         else alert("Please enter a valid move");
     }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice === "rock") {
+                console.log("It's a tie!");
+            } else if (computerChoice === "paper") {
+                ++computerScore;
+                console.log("Paper covers rock. You lose");
+            } else if (computerChoice === "scissors") {
+                ++humanScore;
+                console.log("Rock breaks the scissors. You win!");
+            }
+            break;
+        case "paper":
+            if (computerChoice === "rock") {
+                ++humanScore;
+                console.log("Paper covers rock. You win!");
+            } else if (computerChoice === "paper") {
+                console.log("It's a tie!");
+            } else if (computerChoice === "scissors") {
+                ++computerScore;
+                console.log("Scissors cut throught the paper. You lose");
+            }
+            break;
+        case "scissors":
+            if (computerChoice === "rock") {
+                ++computerScore;
+                console.log("Rock breaks the scissors. You lose");
+            } else if (computerChoice === "paper") {
+                ++humanScore;
+                console.log("Scissors cut throught the paper. You win!");
+            } else if (computerChoice === "scissors") {
+                console.log("It's a tie!");
+            }
+            break;
+    }
+    return;
+}

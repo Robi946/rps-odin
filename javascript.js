d7aa6e5bd1163fc01b678d4ca33b3a121e30a89d
Robi946 <179209963+Robi946@users.noreply.github.com>
2026-09-05 16:13:31 +0300

    Add logic to play entire game

--- javascript.js
+++ javascript.js
@@ -17,44 +17,55 @@ function getHumanChoice() {
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    switch (humanChoice) {
-        case "rock":
-            if (computerChoice === "rock") {
-                console.log("It's a tie!");
-            } else if (computerChoice === "paper") {
-                ++computerScore;
-                console.log("Paper covers rock. You lose");
-            } else if (computerChoice === "scissors") {
-                ++humanScore;
-                console.log("Rock breaks the scissors. You win!");
-            }
-            break;
-        case "paper":
-            if (computerChoice === "rock") {
-                ++humanScore;
-                console.log("Paper covers rock. You win!");
-            } else if (computerChoice === "paper") {
-                console.log("It's a tie!");
-            } else if (computerChoice === "scissors") {
-                ++computerScore;
-                console.log("Scissors cut throught the paper. You lose");
-            }
-            break;
-        case "scissors":
-            if (computerChoice === "rock") {
-                ++computerScore;
-                console.log("Rock breaks the scissors. You lose");
-            } else if (computerChoice === "paper") {
-                ++humanScore;
-                console.log("Scissors cut throught the paper. You win!");
-            } else if (computerChoice === "scissors") {
-                console.log("It's a tie!");
-            }
-            break;
+    function playRound(humanChoice, computerChoice) {
+        switch (humanChoice) {
+            case "rock":
+                if (computerChoice === "rock") {
+                    console.log("It's a tie!");
+                } else if (computerChoice === "paper") {
+                    ++computerScore;
+                    console.log("Paper covers rock. You lose");
+                } else if (computerChoice === "scissors") {
+                    ++humanScore;
+                    console.log("Rock breaks the scissors. You win!");
+                }
+                break;
+            case "paper":
+                if (computerChoice === "rock") {
+                    ++humanScore;
+                    console.log("Paper covers rock. You win!");
+                } else if (computerChoice === "paper") {
+                    console.log("It's a tie!");
+                } else if (computerChoice === "scissors") {
+                    ++computerScore;
+                    console.log("Scissors cut throught the paper. You lose");
+                }
+                break;
+            case "scissors":
+                if (computerChoice === "rock") {
+                    ++computerScore;
+                    console.log("Rock breaks the scissors. You lose");
+                } else if (computerChoice === "paper") {
+                    ++humanScore;
+                    console.log("Scissors cut throught the paper. You win!");
+                } else if (computerChoice === "scissors") {
+                    console.log("It's a tie!");
+                }
+                break;
+        }
+        return;
     }
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if ((humanScore = computerScore)) console.log("It's a tie overall!");
+    else if (humanScore > computerScore) console.log("You win overall!!!!!");
+    else if (humanScore < computerScore) console.log("You lose overall");
     return;
 }
